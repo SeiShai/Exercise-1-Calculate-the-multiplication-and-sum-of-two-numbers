66e1d97efecba931ab1025eeaeab1551809c3be6
Shai_Base <shaibase1328@gmail.com>
2024-01-27 11:13:12 +0800

    fixed less than or equal to and added printing of results

--- calculate product and sum.py
+++ calculate product and sum.py
@@ -11,9 +11,10 @@ second_number = int(input('second number = '))
 def product_or_sum(first_number,second_number):
     product = first_number * second_number
 
-    if product =< 1000:
+    if product <= 1000:
         return product
     else:
         return first_number + second_number
 
 # printing
+print('result:',int(product_or_sum(first_number,second_number)))
