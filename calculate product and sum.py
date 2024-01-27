3959d443c5d7320a2b2c6b40995ff6290f56db89
Shai_Base <shaibase1328@gmail.com>
2024-01-27 11:05:03 +0800

    added inputs for numbers

--- calculate product and sum.py
+++ calculate product and sum.py
@@ -2,10 +2,18 @@
 # return their product only if the product is equal to or lower than 1000.
 # Otherwise, return their sum.
 
+# inputs
+first_number = int(input('first number = '))
+second_number = int(input('second number = '))
+
+
 # def function
 def product_or_sum(first_number,second_number):
     product = first_number * second_number
 
     if product =< 1000:
         return product
-    
+    else:
+        return first_number + second_number
+
+# printing
